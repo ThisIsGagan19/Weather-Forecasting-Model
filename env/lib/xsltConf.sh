013a4653928c9803539e45703d858161ccadc1ca
ThisIsGagan19 <thisisgagan19@gmail.com>
2024-09-04 11:07:23 +0530

    weather forecasting model update changes

---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/gagan/project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/gagan/project/env/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/gagan/project/env/include"
+MODULE_VERSION="xslt-1.1.37"
